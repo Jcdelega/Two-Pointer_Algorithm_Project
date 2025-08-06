1d811328730174e4fdf4420c5d195f993f9c552f
Jose Meza <jcdlega@gmail.com>
2025-08-06 16:07:30 -0600

    Ci: guests file added to the js folder, type changed to module in the HTML5 header

---
+++ src/js/guests.js
@@ -0,0 +1,21 @@
+const guests = [
+  "Ana",
+  "Carmen",
+  "Carlos",
+  "Cecilia",
+  "Daniel",
+  "Diana",
+  "Diego",
+  "Eduardo",
+  "Felipe",
+  "Gabriela",
+  "Hector",
+  "Isabel",
+  "Javier",
+  "Laura",
+  "Mateo",
+  "Natalia",
+  "Paula",
+  "Pedro"
+];
+export default guests
